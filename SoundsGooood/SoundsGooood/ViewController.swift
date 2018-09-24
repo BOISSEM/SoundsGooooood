f3a20c6f66d2c9e61f6255d3116f89677e7cf4dc
Martin <boissemartin@gmail.com>
2018-09-24 20:20:48 +0200

    Add files via upload

---
+++ SoundsGooood/SoundsGooood/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SoundsGooood
+//
+//  Created by Martin Boisse on 27/04/2018.
+//  Copyright © 2018 Martin Boisse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
